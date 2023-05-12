09e0b235382bb1378e162d879b86636c778422f2
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2023-05-12 10:09:03 +0300

    update

--- Assets/AIДоп/Scripts/EnemyController.cs
+++ Assets/AIДоп/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -11,6 +12,7 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private LayerMask _playerMask;
     [SerializeField] private float _sightRange;
     [SerializeField] private float _walkRange;
+    [SerializeField] private float _approachRange;
 
     private NavMeshAgent _navMeshAgent;
 
@@ -32,6 +34,19 @@ public class EnemyController : MonoBehaviour
         {
             Patrol();
         }
+        Vector3 direction = _playerTranform.position - transform.position;
+        float angleInDegrees = 30f; // Góc 30 độ
+        Quaternion rotationQuaternion = Quaternion.Euler(0f, Random.Range(-angleInDegrees, angleInDegrees), 0f);
+
+
+        Vector3 rotatedDirection = rotationQuaternion * direction.normalized * Random.Range(0f, _approachRange);
+
+        Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f),0f, Random.Range(-1f, 1f));
+        Vector3 randomPosition = rotatedDirection + randomDirection;
+
+        Debug.DrawLine(transform.position, rotatedDirection, Color.red);
+        Debug.DrawLine(randomPosition, randomPosition + Vector3.up, Color.red, 3f);
+
     }
 
     private void Patrol()
@@ -51,11 +66,32 @@ public class EnemyController : MonoBehaviour
 
     private void SearchPoint()
     {
-        float randomOffsetX = Random.Range(-_walkRange, _walkRange);
-        float randomOffsetZ = Random.Range(-_walkRange,_walkRange);
 
-        _walkPoint = new Vector3(transform.position.x + randomOffsetX, transform.position.y, transform.position.z + randomOffsetZ);
+        Vector3 direction = _playerTranform.position - transform.position;
 
+
+        bool player = Physics.Raycast(transform.position, direction, _approachRange, _playerMask);
+        print(player);
+        if(player)
+        {
+            //float angleInDegrees = 30f; // Góc 30 độ
+            //Quaternion rotationQuaternion = Quaternion.Euler(0f, Random.Range(-angleInDegrees, angleInDegrees), 0f);
+
+
+            //Vector3 rotatedDirection = rotationQuaternion * direction.normalized;
+            //Debug.DrawLine(transform.position, rotatedDirection, Color.red);
+
+
+            //_walkPoint = transform.position + rotatedDirection * _approachRange;
+            //Debug.DrawLine(_walkPoint, _walkPoint + Vector3.up, Color.red, 6f);
+        }
+        else
+        {
+            float randomOffsetX = Random.Range(-_walkRange, _walkRange);
+            float randomOffsetZ = Random.Range(-_walkRange, _walkRange);
+
+            _walkPoint = new Vector3(transform.position.x + randomOffsetX, transform.position.y, transform.position.z + randomOffsetZ);
+        }
         NavMeshPath path = new NavMeshPath();
         _navMeshAgent.CalculatePath(_walkPoint, path);
         if (path.status == NavMeshPathStatus.PathComplete)
@@ -73,6 +109,8 @@ public class EnemyController : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, _sightRange);
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireCube(transform.position, new Vector3(_walkRange * 2, transform.position.y, _walkRange * 2));
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _approachRange);
     }
 
     private void MoveToTarget(Vector3 position)
@@ -88,17 +126,14 @@ public class EnemyController : MonoBehaviour
         Debug.DrawLine(transform.position + Vector3.up, transform.position + direction.normalized * direction.magnitude, Color.blue);
 
         bool player = Physics.Raycast(transform.position, direction, _sightRange, _playerMask);
-        bool obstacle = Physics.Raycast(transform.position, direction, direction.magnitude, _playerMask);
-
-        return !(!player | obstacle);
-    }
-
-    private void SetDestinaltionToMousePosition()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        bool obstacle = Physics.Raycast(transform.position, direction, direction.magnitude,
+            ~_playerMask);
+        Debug.DrawRay(transform.position, direction,Color.green);
+        if (!(!player | obstacle))
         {
-            _navMeshAgent.SetDestination(hit.point);
+            print("player: " + player);
+            print("obstacle: " + obstacle);
         }
+        return !(!player | obstacle);
     }
 }
